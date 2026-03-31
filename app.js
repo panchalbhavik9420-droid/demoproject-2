51769bf01863ae29f390063e2a80eba92866f913
panchalbhavik9420-droid <panchalbhavik9420@gmail.com>
2026-03-31 10:10:59 +0530

    some minor changes

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 let h1=document.querySelector("h1");
 let btn=document.querySelector("button");
+let p=document.querySelector("p");
+let inp=document.querySelector("input");
 btn.addEventListener("click",()=>{
     h1.innerHTML="<b>ram is happy</b>";
+    p.innerHTML=`<i><b>${inp.value}</b></i>`
 })
